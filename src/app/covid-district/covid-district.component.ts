8c00ce1a602c2284c1008be06fb78e3532fec23f
girish2693 <girishsai2693@gmail.com>
2020-07-19 17:51:30 +0530

    testing git

--- src/app/covid-district/covid-district.component.ts
+++ src/app/covid-district/covid-district.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DistrictService } from '../district.service';
+import { Idistr } from '../distri';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-covid-district',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CovidDistrictComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dserv: DistrictService) { }
+
+  public res : Idistr;
+  public emsg;
+
+
 
   ngOnInit(): void {
+    this.dserv.getDistrict().subscribe(data=>this.res = data,error=>this.emsg=error);
   }
 
 }
