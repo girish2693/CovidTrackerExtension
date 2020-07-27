cc75cfc44d09f0854c1c27eec759839b01c81f80
girish2693 <girishsai2693@gmail.com>
2020-07-27 17:37:06 +0530

    displayed state in dropdown

--- src/app/covid-district/covid-district.component.ts
+++ src/app/covid-district/covid-district.component.ts
@@ -14,12 +14,25 @@ export class CovidDistrictComponent implements OnInit {
   public res : Idistr[];
   public emsg;
   public st:any=[];
+  public selected=null;
+  public val : any=[];
+  public select = null;
+  public flag = false;
 
 
 
   ngOnInit(): void {
     this.dserv.getState().subscribe(data=>this.st=data,error=>this.emsg=error)
     // this.dserv.getDistrict().subscribe(data=>this.res = data,error=>this.emsg=error);
+  }
+
+  public onChange(event){
+    for( this.val of this.st){
+      if(this.val.statecode==event){
+        this.selected = this.val;
+        this.flag = true;
+      }
+    }
   }
 
 }
